c87ae461c45b8e49cbf8e9a73e278844ecfbcf33
Gunnar Arnarsson <github@arnarsson.net>
2025-10-28 22:24:27 +0000

    fix up linting, add to ci

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,13 @@
-try {
-  const prettier = require('prettier');
-  if (prettier && typeof prettier.resolveConfig === 'object' && typeof prettier.resolveConfig.sync !== 'function') {
-    prettier.resolveConfig.sync = () => null;
-  }
-} catch (err) {
-  // ignore if prettier is not available
-}
-
 module.exports = {
   root: true,
   extends: '@react-native-community/eslint-config',
+  plugins: ['prettier'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'prettier/prettier': 'off',
     'react-native/no-inline-styles': 'off',
     curly: 'off',
     'no-unused-vars': ['warn', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+    'prettier/prettier': 'off',
   },
   overrides: [
     {
